ccda9224b1a60c24e6559b342ef44b2aa0fd475d
Addie Jordon <addie@Addies-MacBook-Pro.local>
2025-03-20 12:42:37 +0100

    it's alive

--- xyz2tab.py
+++ xyz2tab.py
@@ -25,8 +25,10 @@ def classify_bond(pair, bond_length):
 
     match = re.search(r"^([a-zA-Z]+)(\d+)", atom1)
     atom1_label = match.group(1)
+    atom1_id = match.group(2)
     match = re.search(r"^([a-zA-Z]+)(\d+)", atom2)
     atom2_label = match.group(1)
+    atom2_id = match.group(2)
 
     if atom1_label < atom2_label:
         bond_key = atom1_label + "-" + atom2_label
@@ -40,14 +42,33 @@ def classify_bond(pair, bond_length):
         if length_diff < abs(bond_length_dict[closest] - bond_length):
             closest = order
 
-    return [atom1, atom2, bond_length, bond_length_dict[closest], closest]
+    return (atom1_label, atom1_id, atom2_label, atom2_id, closest)
+
+# table of type: 
+# atom1 label, atom1 id, atom2 label, atom2 id, bond order
+def table_to_gml(table):
+    gml_bond_char = {'single': '-', 'double':'=', 'triple':'#'}
+    lines = ["graph ["]
+    nodes = []
+    for (atom1_label, atom1_id, atom2_label, atom2_id, order) in table:
+        if atom1_id not in nodes:
+            lines.append(f"\tnode [ id {atom1_id} label \"{atom1_label}\" ]")
+            nodes.append(atom1_id)
+        if atom2_id not in nodes:
+            lines.append(f"\tnode [ id {atom2_id} label \"{atom2_label}\" ]")
+            nodes.append(atom2_id)
+
+        lines.append(f"\tedge [ source {atom1_id} target {atom2_id} label \"{gml_bond_char[order]}\" ]")
+
+    lines.append("]")
+    s = "\n".join(lines)
+    print(s)
 
 def main():
     args = parse_args()
     
     #for windows console
     sys.stdout.reconfigure(encoding='utf-8')  
-
 
     pt = PrintTab(args)
     pt.print_sel_dist_table()
@@ -57,7 +78,7 @@ def main():
 
     bond_table = pt.pr_sel_dist
     classification = [classify_bond(x,y) for (x,y) in zip(bond_table['A-B'], bond_table['distance_calc'])]
-    [print(x) for x in classification]
+    table_to_gml(classification)
 
     # cp = CreatePlot(args, pt.xyz_df, pt.sel_dist2)
     
