f57c6f2a9343721fe453afad5b942677f0bb0e98
Addie Jordon <addie@Addies-MacBook-Pro.local>
2025-03-21 13:20:49 +0100

    debug

--- xyz2tab.py
+++ xyz2tab.py
@@ -75,7 +75,7 @@ def write_gml_file(pt, filename="unnamed") -> Graph:
         classification = [classify_bond(x,y) for (x,y) in zip(bond_table['A-B'], bond_table['distance_calc'])]
         gml_string = table_to_gml(classification)
     else: #there is only a single atom, so could not make any bond information
-        element = pt.xyz_df[['element']][0]
+        element = pt.xyz_df.iloc[0]['element']
         gml_string = f"graph [\n\tnode [ id 0 label {element} ]\n]"
     try:
         mod_graph = mod.Graph.fromGMLString(gml_string)
