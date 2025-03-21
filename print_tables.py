ab00c1f8d7b6c84d2b96a09efa986d8787eabeb4
Addie Jordon <addie@Addies-MacBook-Pro.local>
2025-03-21 13:00:31 +0100

    debug

--- print_tables.py
+++ print_tables.py
@@ -128,7 +128,8 @@ class PrintTab:
         #C1 1.1 0.0 1.5
         #C2 2.3 1.5 0.0
         #iloc [:,3:6] contains xyz coordinates
-        print(xyz_df)
+        print(f"xyz_df: {xyz_df}")
+        print(f"iloc: {xyz_df.iloc[:,3:6]}")
         dist_mat_full=pd.DataFrame(squareform(pdist(xyz_df.iloc[:,3:6],'euclid')),
               columns = xyz_df[['atom1_idx','element','cov_radius']],
               index = xyz_df[['atom2_idx','element','cov_radius']])
