f9dcd218574f3643563da2a97576ef1d582abc1c
Addie Jordon <addie@Addies-MacBook-Pro.local>
2025-04-07 12:53:22 +0200

    is it just chem that seg faults?

--- xyz2tab.py
+++ xyz2tab.py
@@ -15,6 +15,7 @@ import mod
 #from mpl_toolkits.mplot3d import proj3d                         #for fancy arrows in xyz
 from os import path
 from pathlib import Path
+import rdkit
 # from rdkit import Chem
 
 from graph import Graph
